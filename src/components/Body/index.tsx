066ea4b65993a17bd2daeb49629610bedb77acf5
lobertski <lopezrobertjoshua@gmail.com>
2022-02-19 12:27:28 +0800

    Some Changes

---
+++ src/components/Body/index.tsx
@@ -0,0 +1,14 @@
+import { TextField, Button } from "@mui/material";
+import React from "react";
+
+const Body: React.FC<any> = (props) => {
+  return (
+    <>
+      <TextField />
+
+      <Button />
+    </>
+  );
+};
+
+export default Body;
